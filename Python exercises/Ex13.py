a06612254d31b6331111adaca4dacb849d8da1a7
malikmubeen <malikmubeen1@gmail.com>
2016-12-07 16:12:44 -0500

    12/7/16

---
+++ Python exercises/Ex13.py
@@ -0,0 +1,13 @@
+# Ex 13
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:",script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+z = raw_input("Enter your fourth variable name: ")
+print "Fourth variable: ", z
